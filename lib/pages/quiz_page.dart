cf29b78e201039309cca1616d97b0e58a57e0465
fareesa <fareesamasroor@gmail.com>
2019-03-03 19:03:13 +0530

    first commit

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
+
+import './score_page.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Java is platform independent", true),
+    new Question("Java is 100% object-oriented", false),
+    new Question("Wrapper classes converts java primitives into reference types", true),
+    new Question("Array list is synchronized", false),
+    new Question("Vector defines the increement size", true),
+    new Question("Stack memory can be  accessed by other threads,", false),
+    new Question("Runtime polymorphism is done using inheritance and interface", true),
+    new Question("An interface can contain constructors", false),
+    new Question("Method overloading is a compile time polymorphism", true),
+    new Question("Single class cannot implement several interfaces", false),
+    new Question("Java doesn't support multiple inheritance", true),
+    new Question("Cookies work in servlets", true),
+    new Question("All servlets must implement the javax.servlet.Servlet interface", true),
+    new Question("In java a private or a static method can be overridden", false),
+
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column( // This is our main page
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)), //true button
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)), // false button
+          ],
+        ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+          isCorrect,
+          () {
+            if (quiz.length == questionNumber) {
+              Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new ScorePage(quiz.score, quiz.length)), (Route route) => route == null);
+              return;
+            }
+            currentQuestion = quiz.nextQuestion;
+            this.setState(() {
+              overlayShouldBeVisible = false;
+              questionText = currentQuestion.question;
+              questionNumber = quiz.questionNumber;
+            });
+          }
+        ) : new Container()
+      ],
+    );
+  }
+}
